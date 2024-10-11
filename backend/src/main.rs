9d949750d4fe096a7f2e82964ca031ecbed45d68
darren277 <darren277@yahoo.com>
2024-10-11 08:51:31 -0700

    Main code.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,30 +1,28 @@
-use hyper::{Body, Request, Response, Server, StatusCode};
-use hyper::service::{make_service_fn, service_fn};
-use std::convert::Infallible;
+use tokio::{io::AsyncWriteExt, net::TcpListener};
 
-async fn handle_request(req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    // Purely process request and generate response
-    let response = match (req.method(), req.uri().path()) {
-        (&hyper::Method::GET, "/") => Response::new(Body::from("Hello, World!")),
-        _ => Response::builder()
-            .status(StatusCode::NOT_FOUND)
-            .body(Body::from("Not Found"))
-            .unwrap(),
-    };
-
-    Ok(response)
-}
+use std::io;
 
 #[tokio::main]
-async fn main() {
-    let addr = ([127, 0, 0, 1], 3000).into();
-    let make_svc = make_service_fn(|_conn| async {
-        Ok::<_, Infallible>(service_fn(handle_request))
-    });
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
 
-    let server = Server::bind(&addr).serve(make_svc);
+    println!("Server running on 127.0.0.1:8080");
 
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
+    loop {
+        match listener.accept().await {
+            Ok((mut socket, addr)) => {
+                println!("New client: {:?}", addr);
+                tokio::spawn(async move {
+                    let response = b"HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: 25\r\n\r\n{\"data\": \"Hello, World!\"}";
+
+                    if let Err(e) = socket.write_all(response).await {
+                        println!("Failed to write to socket: {:?}", e);
+                    }
+
+                    println!("Connection closed");
+                });
+            }
+            Err(e) => println!("Couldn't get client: {:?}", e),
+        }
     }
 }
